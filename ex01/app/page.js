da0fff797350f91cd9c2f705c512d0d6cedc5332
carlinhosborba <161654338+carlinhosborba@users.noreply.github.com>
2025-02-14 00:44:36 +0000

    aula2

---
+++ ex01/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+function Texto1() {
+  return <p>Um outro parágrafo de texto!</p>;
+}
+
+export default function Home() {
+  const aula = 2;
+  return (
+  <div>
+    <h1>Olá Turma!!!</h1>
+    <p>Essa é a nossa {aula}ª aula de React!</p>
+    <Texto1 />
+    <Texto1 />
+  </div>
+  );
+}
