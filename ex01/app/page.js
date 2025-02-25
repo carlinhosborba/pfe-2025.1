cdb9deeff7dfc06500feaebbbdb44ff9c27a3c92
carlinhosborba <161654338+carlinhosborba@users.noreply.github.com>
2025-02-25 00:38:10 +0000

    aula2

--- ex01/app/page.js
+++ ex01/app/page.js
@@ -1,16 +1,29 @@
+"use client"
+
+import { useState } from 'react';
+
 import Image from "next/image";
 function Texto1() {
   return <p>Um outro parágrafo de texto!</p>;
 }
 
 export default function Home() {
+  const [hide, setHide] = useState(false);
+  const [random, setRandom] = useState(Math.floor(Math.random() * 100));
   const aula = 2;
   return (
   <div>
     <h1>Olá Turma!!!</h1>
     <p>Essa é a nossa {aula}ª aula de React!</p>
+    <hr />
+    <button onClick={() => setHide(!hide)}>{hide ? "Show" : "Hide"}</button>
+    <hr />
+    {!hide && (
+      <>
     <Texto1 />
     <Texto1 />
+    </>
+    )}
   </div>
   );
 }
