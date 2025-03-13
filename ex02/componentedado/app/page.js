97add1a36b2e8ad863047b4eebf3b7843737fb17
carlinhosborba <161654338+carlinhosborba@users.noreply.github.com>
2025-03-13 23:59:57 +0000

    correcao do dado

--- ex02/componentedado/app/page.js
+++ ex02/componentedado/app/page.js
@@ -11,11 +11,11 @@ export default function Home() {
 
   const imagens = {
     1: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Dice-1-b.svg",
-    2: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Dice-2-b.svg",
-    3: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Dice-3-b.svg",
-    4: "https://upload.wikimedia.org/wikipedia/commons/0/08/Dice-4-b.svg",
+    2: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Dice-2-b.svg",
+    3: "https://upload.wikimedia.org/wikipedia/commons/b/b1/Dice-3-b.svg",
+    4: "https://upload.wikimedia.org/wikipedia/commons/f/fd/Dice-4-b.svg",
     5: "https://upload.wikimedia.org/wikipedia/commons/0/08/Dice-5-b.svg",
-    6: "https://upload.wikimedia.org/wikipedia/commons/a/a0/Dice-6-b.svg",
+    6: "https://upload.wikimedia.org/wikipedia/commons/2/26/Dice-6-b.svg",
   };
 
   return (
